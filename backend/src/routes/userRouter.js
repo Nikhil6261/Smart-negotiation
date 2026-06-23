04daf4e945e332bead7201e65fd5012e3225bdc7
Nikhil Ahirwar <nikkuahirwar2@gmail.com>
2026-06-23 12:41:07 +0530

    register api

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {login, register} from '../controller/user.js'
-
+import {authlimit} from '../middleware/rateLimiter.js'
 
 
 
@@ -8,7 +8,7 @@ export const userRouter = express.Router()
 
 
 
-userRouter.post('/register',register)
+userRouter.post('/register', authlimit,register)
 userRouter.post('/login',login)
 
 
