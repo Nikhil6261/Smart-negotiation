b6743d4997f7398e602a1b0a70bd7ca48407d2d9
Nikhil Ahirwar <nikkuahirwar2@gmail.com>
2026-06-29 23:50:25 +0530

    api

--- frontend/src/Api/Axios.jsx
+++ frontend/src/Api/Axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000" ,
-  // baseURL: "https://smart-negotiation.onrender.com" ,
+  // baseURL: "http://localhost:3000" ,
+  baseURL: "https://smart-negotiation.onrender.com" ,
   headers: {
     "Content-Type": "application/json",
   },
